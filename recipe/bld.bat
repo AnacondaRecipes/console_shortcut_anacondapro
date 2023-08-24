743e4e3b2a98c0e6f0379cf682121d7ca332d8fb
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-08-24 16:06:20 -0400

    Changed menu item json filename to not conflict with official version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,6 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 copy %RECIPE_DIR%\Iconleak-Atrous-Console.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
-copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\console_shortcut.json
+copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\console_shortcut_anacondapro.json
 if errorlevel 1 exit 1
 
